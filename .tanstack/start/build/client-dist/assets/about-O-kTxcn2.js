006a1903aa35446ecaf6af15329f95c86f97c03d
honokiyuto <honokiyuto@team-lab.com>
2025-07-12 18:18:47 +0900

    reset tanstack

--- .tanstack/start/build/client-dist/assets/about-O-kTxcn2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./main-BP4lLnRT.js";const n=function(){return o.jsx("div",{className:"text-red-500",children:'Hello "/about"!'})};export{n as component};
--- src/routes/about.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/about')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div className="text-red-500">Hello "/about"!</div>
-}
