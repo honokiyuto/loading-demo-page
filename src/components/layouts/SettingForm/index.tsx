f5fcb72bb38ca7b0563bcc8db4f313849ca3dcf8
honokiyuto <honokiyuto@team-lab.com>
2025-08-24 12:52:44 +0900

    chore: 不要なimport削除

--- src/components/layouts/SettingForm/index.tsx
+++ src/components/layouts/SettingForm/index.tsx
@@ -1,5 +1,4 @@
 import { Slider } from '@/components/ui/slider';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   getIntervalFromLocalStorage,
   getSkeletonPatternFromLocalStorage,
